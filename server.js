ff526faa8cae9c9c9bf04e706aa5cf8eba7a5174
CauldenLoyd <97518430+CauldenLoyd@users.noreply.github.com>
2022-01-11 04:50:41 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+var colors = require('colors');
+
+app.get('/', function (request, response) {
+    response.sendFile('/index.html')
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log('[CSLOG]'.rainbow.bold + ' Wait a minute? Could it be? It is!'.brightBlue)
+  console.log(`[CSLOG]`.rainbow.bold + ` Starting up Caulden's Skateboards servers...`.brightGreen);
+  console.log(`[CSLOG]`.rainbow.bold + ` Servers started up successfully!`.brightGreen);
+  console.log(`[CSLOG]`.rainbow.bold + ` Project is online at port ${listener.address().port}`.brightGreen);
+});
